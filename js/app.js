51ea3a1d5a6db17443f1eeb714586d553c541b9e
Vlad Dobrovolsky <vladislavdobrovolskij@MacBook-Air-Vladislav-2.local>
2023-02-20 18:12:11 +0400

    ready

--- js/app.js
+++ js/app.js
@@ -1,31 +1,27 @@
-document.addEventListener('DOMContentLoaded', (e)=>{
-    e.preventDefault()
+document.addEventListener('DOMContentLoaded', (e) => {
+    e.preventDefault();
     const element = document.querySelector('.js-choice');
 
-const choices = new Choices(element, {
-    searchEnabled: false,
-    itemSelectText: "",
-    allowHTML: true
-})
+    const choices = new Choices(element, {
+        searchEnabled: false,
+        itemSelectText: "",
+        allowHTML: true
+    })
 
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+    const swiper = new Swiper('.swiper', {
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-pagination',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-  
-  });
+        direction: 'horizontal',
+        loop: true,
+
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        navigation: {
+            nextEl: '.swiper-pagination',
+        },
+
+
+    });
 })
